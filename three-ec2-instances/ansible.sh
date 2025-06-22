2cebdc779c44b32b94a5422c3e846275cd83a014
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-22 17:03:22 +0530

    expense ansible roles common component vault

--- three-ec2-instances/ansible.sh
+++ three-ec2-instances/ansible.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 dnf install ansible -y
-
-# Clone your Ansible repo
 cd /tmp
 git clone https://github.com/Lingaiahthammisetti/4.8.expense-ansible-roles-common-component-vault.git
 cd 4.8.expense-ansible-roles-common-component-vault
@@ -12,14 +9,10 @@ mkdir -p /etc/ansible
 echo 'admin1234' > /etc/ansible/.vault_pass.txt
 chmod 600 /etc/ansible/.vault_pass.txt
 
-# Run playbooks using the Vault password file
-# ansible-playbook db.yaml --vault-password-file /etc/ansible/.vault_pass.txt
-
 # Run component playbooks
 for component in db db backend frontend; do
   ansible-playbook main.yaml -e "component=$component" --vault-password-file /etc/ansible/.vault_pass.txt
 done
-
 
 
 # #!/bin/bash
@@ -27,7 +20,6 @@ done
 # cd /tmp
 # git clone https://github.com/Lingaiahthammisetti/4.8.expense-ansible-roles-common-component-vault.git
 # cd 4.8.expense-ansible-roles-common-component-vault
-
 # ansible-playbook main.yaml -e component=db
 # ansible-playbook main.yaml -e component=db #Run twice db.yaml
 # ansible-playbook main.yaml -e component=backend
